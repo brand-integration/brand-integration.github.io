ebe73003d127b2b97b173d757c41388ca77e1082
brand-integration <107556713+brand-integration@users.noreply.github.com>
2023-05-04 09:44:22 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://brand-integration.github.io',
-  base: '/integrationsbolaget',
+  site: 'https://integrationsbolaget.com'
 })
